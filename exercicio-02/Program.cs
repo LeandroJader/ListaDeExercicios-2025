98ba7f0eacbd38b912f1b1c5fb4aa9dfa91b70f0
leandroJader <ldandro>
2025-03-23 19:08:52 -0300

    exercicio para calcular o volume de um cilindro

---
+++ exercicio-02/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Cacular o volume de um cilindro ");
+            Console.WriteLine("================================");
+
+            Console.WriteLine("digite o raio do cilindo ");
+            double raio = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("digite a altura do cilindro ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            double volume = Math.PI * Math.Pow(raio, 2) * altura;
+
+            Console.WriteLine($"O volume do cilindo e de {volume} cm ³ ");
+
+
+        }
+    }
+}
