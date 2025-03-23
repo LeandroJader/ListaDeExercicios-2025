b2e2fef0ce312284378369488667122bf90ff59c
leandroJader <ldandro>
2025-03-23 19:06:34 -0300

    exercicio para calcular o volume de uma caixa retangular

--- Exercicio-01/Program.cs
+++ Exercicio-01/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Exercicio_01
+﻿namespace exercicio01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("=========================");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Calculando o volume de uma caixa retangular");
+            Console.ResetColor();
+            Console.WriteLine("=========================");
+
+            Console.WriteLine("Digite o comprimento do retangulo");
+            decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Digite a altura do retangulo");
+            decimal altura = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Digite a largura do retangulo");
+            decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+
+            decimal volume = 0;
+
+
+            volume = comprimento * largura * altura;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"o volume e de {volume} cm ³ ");
+            Console.ResetColor();
+            Console.ReadLine();
+
         }
     }
 }
