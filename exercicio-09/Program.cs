e44dc326a4ecc239f72f28dc1599d1321313d5e9
leandroJader <ldandro>
2025-03-23 19:38:35 -0300

    exercicio para ler as dimensões de um terreno

---
+++ exercicio-09/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace exercicio_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==============================");
+            Console.WriteLine("Ler as dimensões de um terreno");
+            Console.WriteLine("==============================");
+
+            Console.WriteLine("digite a largura do terreno ");
+            double largura  = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("digite o comprimento do terreno ");
+            double comprimento  = Convert.ToDouble(Console.ReadLine());
+
+            double area = largura * comprimento;
+
+            Console.WriteLine($"seu terreno contem {area} m²");
+            Console.ReadLine();
+        }
+    }
+}
