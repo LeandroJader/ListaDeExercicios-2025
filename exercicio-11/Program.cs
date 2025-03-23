647ab1b62c11ed9297427c779bbc941ee932c9b1
leandroJader <ldandro>
2025-03-23 20:28:13 -0300

    exercicio para calcular quantos dias uma pessoa ja viveu.

---
+++ exercicio-11/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace exercicio_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Calcular dias vividos");
+            Console.WriteLine("---------------------");
+
+            Console.WriteLine("informe seu nome ");
+            string nome = Console.ReadLine()!;
+
+            Console.WriteLine("informe sua idade");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine($"{nome}, você viveu cerca de {idade*365} dias ");
+
+            Console.ReadLine();
+
+        }
+    }
+}
