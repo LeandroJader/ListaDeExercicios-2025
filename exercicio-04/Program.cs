ae6a27db2d2df6cfc0d9e54ac6e8892f5bf4dc28
leandroJader <ldandro>
2025-03-23 19:12:58 -0300

    exercicio para converter graus Celcius para Fahrenheit

---
+++ exercicio-04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace exercicio04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("===========================================");
+            Console.WriteLine("Conversão de graus Celsius para  Fahrenheit");
+            Console.WriteLine("============================================");
+
+            Console.WriteLine("digite os graus Celsius ");
+            double GausCelcius = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("===========================================");
+
+            double fahrenheit = (GausCelcius * 1.8) + 32;
+            Console.WriteLine($" {GausCelcius} graus celcius são {fahrenheit} graus fahrenheit ");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
