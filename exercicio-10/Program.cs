5035593bfe9eca819c728b7f7ef2b3ede8386d90
leandroJader <ldandro>
2025-03-23 20:18:27 -0300

    exercicio para calcular a quantidade arrecadada por uma padaria e para estipular um valor para guardar na poupança.

---
+++ exercicio-10/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace exercicio_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===============================");
+            Console.WriteLine("Sistema de calculo para padaria");
+            Console.WriteLine("===============================");
+
+            double Valorpaes =0.12;
+            double Valorbroas = 1.50;
+            double ValorArrecadadopaes = 0;
+            double ValorArrecadadoBroas = 0;
+            double poupanca = 0;
+
+            Console.WriteLine("informe a quantidade de pães vendidos");
+            int quantidadepaes = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("=======================================");
+
+            Console.WriteLine("informe a quantidade de broas vendias");
+            int quantidadebroas = Convert.ToInt32(Console.ReadLine());
+
+            ValorArrecadadoBroas = quantidadebroas * Valorbroas;
+            ValorArrecadadopaes = quantidadepaes * Valorpaes;
+            poupanca = ValorArrecadadoBroas + ValorArrecadadopaes;
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine($"O valor total arrecadado foi de {ValorArrecadadoBroas+ValorArrecadadopaes}R$");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine($"O valor a ser depositado na poupança e {poupanca*0.1:f2}R$");
+            
+            Console.ReadLine();
+        }
+    }
+}
