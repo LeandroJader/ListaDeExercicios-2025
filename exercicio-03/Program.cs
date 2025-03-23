61de43d2bf460c6f1d30bd14a5d30d6b42956745
leandroJader <ldandro>
2025-03-23 19:11:06 -0300

    exercicio para calcular a média de combustivel

---
+++ exercicio-03/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace exercicio_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcular Media de combustivel");
+            Console.WriteLine("=============================");
+            Console.WriteLine("informe a km inicial do veiculo ");
+
+            double Kminicial = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("=============================");
+            Console.WriteLine("informe a quilometragem final ");
+            double kmFinal = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("=============================");
+            Console.WriteLine("informe a quantidade de combustivel consumida ");
+            double consumoCombustivel = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("=============================");
+            Console.WriteLine($"o carro percorreu {(kmFinal - Kminicial)}km e fez um consumo medio por litro de {1 / consumoCombustivel:F3}");
+            Console.ReadLine();
+
+        }
+    }
+}
