4d2114f4d8fd01eac57569dfa1d2a33541b06605
leandroJader <ldandro>
2025-03-23 22:07:55 -0300

    exercicio para axibir os numeros em ordem decrecente

---
+++ exercicio-14/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace exercicio_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Valores em ordem decrecente");
+
+            int[] valoresDecresdecrecente = new int[3];
+
+            Console.WriteLine("insira o primeiro valor");
+            valoresDecresdecrecente[0] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("insira o primeiro valor");
+            valoresDecresdecrecente[1] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("insira o terceiro valor");
+            valoresDecresdecrecente[2] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("-----------------------");
+           
+             var ordenados = valoresDecresdecrecente.OrderByDescending(v => v);
+
+            foreach(var valor  in  ordenados)
+            {
+                Console.WriteLine(valor);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
