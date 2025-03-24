a326f456b06d632fbd28c848fba2c29fac0bb582
leandroJader <ldandro>
2025-03-23 22:58:30 -0300

    exercicio para ler Valores A e B e realizar soma ou multiplicação de ambos

---
+++ exercicio-17/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace exercicio_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorC = 0;
+            Console.WriteLine("------------------");
+            Console.WriteLine("Ler valores A e B ");
+            Console.WriteLine("------------------");
+
+            Console.WriteLine("informe o valor A");
+            int valorA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("informe o valor B");
+            int valorB = Convert.ToInt32(Console.ReadLine());
+
+            if (valorA == valorB)
+            {
+                valorC = valorA + valorB;
+                Console.WriteLine($"O resultado da soma de A e B é :{valorC}");
+
+            }
+            else
+            {
+                valorC = valorA * valorB;
+                Console.WriteLine($"O resultado da multiplicação dos valores de A e B é :{valorC}");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
