0de212c3effd01f74dafe0c9fa096f68c55355e6
leandroJader <ldandro>
2025-03-23 23:11:59 -0300

    exercicio para imprimir números impares entre 100 e 200

---
+++ exercicio-18/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace exercicio_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Imprimir numeros Impares ");
+
+            
+            
+                for (int i= 100; i < 200; i++)
+                {
+                  
+                 if (i % 2 == 0)
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine(i);
+
+                }
+
+                }
+            Console.ReadLine();
+
+
+
+
+            }
+
+
+        }
+    }
+
