9b3bddcb8146a4e949a216f0180c0e38c78d750d
leandroJader <ldandro>
2025-03-24 09:12:10 -0300

    exercicio para ver a tabuada de um número

---
+++ exercicio-20/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace exercicio_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabuada");
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("digite um número para ver a tabuada");
+            Console.WriteLine("-----------------------------------");
+
+            int NumeroTabuada = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i=0; i<11; i++)
+            {
+                int resultado = NumeroTabuada * i;
+                Console.WriteLine($"{NumeroTabuada} x {i} = {resultado}");
+
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
