baa9f93474e7be299b23a5376fa5d327d83f87eb
leandroJader <ldandro>
2025-03-24 09:52:41 -0300

    exercicio Fatorial

---
+++ exercicio-21/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace exercicio_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fatorial");
+
+            Console.WriteLine("Informe o valor fatorial: ");
+            int ValorN = Convert.ToInt32(Console.ReadLine());
+
+            long fatorial = 1;
+            Console.Write($"{ValorN}! = ");
+
+            for (int i = ValorN; i >= 1; i--)
+            {
+                Console.Write(i);
+                if (i > 1)
+                {
+                    Console.Write(" X ");
+                }
+                fatorial *= i;
+            }
+
+            Console.WriteLine($" = {fatorial}");
+            Console.ReadLine();
+        }
+    }
+}
