5272ffc89eea65231eb2864165a4d1920af4af96
leandroJader <ldandro>
2025-03-23 23:27:08 -0300

    exercicio que soma os numeros impares que sao multiplos de tres

---
+++ exercicio-19/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace exercicio_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("somar numeros impares ");
+
+            int soma = 0;
+
+            for (int i = 1; i < 500; i++)
+            {
+
+                if (i % 2!= 0 && i %3==0)
+                {
+                    
+                    soma += i;
+                }
+              
+
+            }
+                    Console.WriteLine($"resultado da soma dos numeros impares e multiplos de tres :{soma}");
+            Console.ReadLine();
+
+
+
+
+        }
+
+
+    }
+}
+
