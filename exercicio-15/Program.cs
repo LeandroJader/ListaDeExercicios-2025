a886bd557da5c322814f60b449d2e55181d1a950
leandroJader <ldandro>
2025-03-23 22:34:55 -0300

    exercicio para calcular o indice de massa muscular

---
+++ exercicio-15/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace exercicio_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Calcular indice de massa muscular");
+            Console.WriteLine("-----------------");
+
+            Console.WriteLine("informe seu peso ");
+            double peso=Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("informe sua altura  ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            double imc = peso / Math.Pow(altura, 2);
+
+            if(imc < 18.5)
+            {
+                Console.WriteLine("Você esta abaixo do peso");
+                }
+
+            else if (imc >= 18.5 && imc < 25) {
+
+                    Console.WriteLine("você esta no peso normal");
+                }
+            else if (imc>=25 && imc<30)
+            {
+                Console.WriteLine("você esta acima do peso");
+            }
+            else if (imc > 30)
+            {
+                Console.WriteLine("você esta obeso");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
