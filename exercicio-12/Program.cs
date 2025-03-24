fe7ce9ddc590d2a9455a364592d76f3f5ba17115
leandroJader <ldandro>
2025-03-23 21:05:34 -0300

    exercicio para calcular o salário final acrescentando aumento e descontando impostos

---
+++ exercicio-12/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace exercicio_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Calcular aumento de salario e impostos");
+            Console.WriteLine("--------------------------------------");
+
+            Console.WriteLine("digite seu salario atual");
+            double salarioAtual = Convert.ToDouble(Console.ReadLine());
+
+            double AumentoSalario = salarioAtual *0.15 ;
+            double impostos = (salarioAtual+AumentoSalario) * 0.08;
+            double salariofinal = salarioAtual + AumentoSalario - impostos;
+
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"seu salario inicial era {salarioAtual}R$");
+            Console.WriteLine($"voce recebeu aumento de 15% seu salario ficou {AumentoSalario+salarioAtual}R$");
+            Console.WriteLine($"salario final descontado 8% de impostos {salariofinal}R$");
+            Console.ReadLine();
+        }
+    }
+}
