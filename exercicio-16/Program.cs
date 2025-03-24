7a28af455e79729905d01cf5f4c8af9dd36f127f
leandroJader <ldandro>
2025-03-23 22:45:15 -0300

    exercicio para verificar se o numero e par ou impar

---
+++ exercicio-16/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace exercicio_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Verificar se o numero e par ou impar");
+            Console.WriteLine("------------------------------------");
+
+            Console.WriteLine("digite um número para verificar se e par ou impar");
+            decimal numero = Convert.ToInt32(Console.ReadLine());
+
+
+            if (numero %2==0) {
+
+                Console.WriteLine("o número e par");
+            
+            }
+            else
+            {
+                Console.WriteLine("o número e impar");
+            }
+            Console.ReadLine();
+        }
+    }
+}
