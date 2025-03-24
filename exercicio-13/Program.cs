cc4887d993dbcb604a56bb512d24ba7d786aaeb7
leandroJader <ldandro>
2025-03-23 21:20:06 -0300

    exercicio para verificar se a soma de A+B é menorr que C

---
+++ exercicio-13/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace exercicio_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ler valores A e B ");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("dgite o valor A");
+            double ValorA= Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("dgite o valor B");
+            double ValorB = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("dgite o valor C");
+            double ValorC = Convert.ToDouble(Console.ReadLine());
+
+            double somavalores = ValorA + ValorB;
+
+            if (somavalores < ValorC)
+            {
+                Console.WriteLine($"O resultado da soma A e B e menor que C");
+
+            }
+            else
+            {
+                Console.WriteLine("O resultado da soma e maior que C");
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
