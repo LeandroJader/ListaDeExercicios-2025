aafb150af195951a60b899692cca69044f7349dc
leandroJader <ldandro>
2025-03-24 10:32:13 -0300

    exercicio sequencia Fibonacci

---
+++ exercicio-22/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace exercicio_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o número até onde deseja ver a sequência de Fibonacci: ");
+            int limite = Convert.ToInt32(Console.ReadLine());
+
+            int primeiro = 0, segundo = 1;
+
+            Console.Write("Sequência de Fibonacci: ");
+            Console.Write(primeiro + " " + segundo + " ");
+
+            while (true)
+            {
+                int proximo = primeiro + segundo;
+                if (proximo > limite)
+                    break;
+
+                Console.Write(proximo + " ");
+                primeiro = segundo;
+                segundo = proximo;
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+       
+    }
+}
